b1ea8cc082e7a39bed6f1a091ca5415df1f714c2
Lamarr Dumas <dumaslamarr@gmail.com>
2020-03-05 18:09:16 -0600

    Implement a route for api/users

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiUsersRouter = require('./routes/api/users');
 
 var app = express();
 var mongoose = require('mongoose');
@@ -28,6 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/api/users', apiUsersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
