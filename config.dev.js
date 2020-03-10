dc5bb555fb7ddbc41a417788fb91128ac2456e14
Lamarr Dumas <dumaslamarr@gmail.com>
2020-03-10 14:31:12 -0500

    Configure session and cookie data

--- config.dev.js
+++ config.dev.js
@@ -4,5 +4,18 @@ module.exports = config;
 
 var config = require('./config.dev');
 
-//Test the file
+//Session configuration object
+config.session = {};
+
+//Cookie configuration object
+config.cookie = {};
+
+//Create a random string to sign the session data
+//Bigger is better, more entropy is better
+//The is OK for dev, change for production
+config.session.secret = '7j&1tH!cr4F*1U';
+
+//Define the domain for which this cookie is to be set
+config.cookie.domain = 'localhost';
+
 console.log(config);
